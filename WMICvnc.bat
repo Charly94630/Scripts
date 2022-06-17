20f85ca0444cee2fae7deeaaa06bc8f7cab9dfef
Charly94630 <97137104+Charly94630@users.noreply.github.com>
2022-06-17 11:26:08 -0300

    Update WMICvnc.bat

--- WMICvnc.bat
+++ WMICvnc.bat
@@ -9,7 +9,7 @@ robocopy "\\nb-cnd-it00\share\VNC " "\\%ip%\c$\VNC " /E
 
 
 
-wmic /user:administrador /password:T4t4nk4m0n!2020 /node:%ip% process call create "cmd.exe /c start /WAIT C:\VNC\vncinstall.bat"
+wmic /user:administrador /password: /node:%ip% process call create "cmd.exe /c start /WAIT C:\VNC\vncinstall.bat"
 net use \\%ip%\c$ /d /y>nul 2>&1
 
 pause
